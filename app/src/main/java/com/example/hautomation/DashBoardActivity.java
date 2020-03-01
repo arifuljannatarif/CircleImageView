f40b6a04aeeb8f9f7e338ddd688b0bc29149eb2e
Arif <cse.ariful@gmail.com>
2020-03-01 12:30:56 +0600

    library cleaned

---
+++ app/src/main/java/com/example/hautomation/DashBoardActivity.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2020. This code is created and written by Ariful Jannat Arif on 3/1/20 12:30 PM
+ */
+
+package com.example.hautomation;
+
+import android.app.Activity;
+
+public class DashBoardActivity extends Activity {
+}
